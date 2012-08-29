2e94bb70bf12aa0c5c7dea915dc6b8301adefe14
Roberto Arista <arista.rob@gmail.com>
2012-08-29 20:31:49 +0200

    text formatting

--- robo_imposer.py
+++ robo_imposer.py
@@ -1,7 +1,12 @@
-###### Robo_imposer v.02
-# Author: Roberto Arista http://robertoarista.it/
+# Copyright (c) 2012 - Roberto Arista (http://robertoarista.it/)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# You need Nodebox (http://nodebox.net/code/index.php/Home) installed on your mac and PyPdf module (http://pybrary.net/pyPdf/) in the ~/Library/Application Support/NodeBox/ folder to use this script (info at this link http://nodebox.net/code/index.php/Library).
+# To use this script check the README file and the instruction folder
 
 ## Open modules
 from pyPdf import PdfFileWriter, PdfFileReader
